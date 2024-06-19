58a8d12927297608db3d3829f6f67993fc17d7de
Divyashri S <divyashri.srinivasadevan@tringapps.net>
2024-06-19 10:40:57 +0530

    commit delete

--- src/modules/mail/mail-config.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MailConfig } from './mail.config';
-
-@Module({
-  providers: [MailConfig],
-  exports: [MailConfig],
-})
-export class MailConfigModule {}
--- src/modules/user-role/user-role.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RoleModule } from './role/role.module';
-
-@Module({
-  imports: [RoleModule],
-})
-export class UserRoleModule {}
