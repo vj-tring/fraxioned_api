af759653cef9008a159d2b1d16b37bd12aca0ce5
Divyashri S <divyashri.srinivasadevan@tringapps.net>
2024-07-23 12:27:51 +0530

    commit 23-07-2024

---
+++ Dump20240723/fraxioned_holidays.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 192.168.1.47    Database: fraxioned
+-- ------------------------------------------------------
+-- Server version	8.0.37
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `holidays`
+--
+
+DROP TABLE IF EXISTS `holidays`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `holidays` (
+  `id` int NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `year_id` int DEFAULT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `year_id_holiday_fk_idx` (`year_id`),
+  CONSTRAINT `year_id_holiday_fk` FOREIGN KEY (`year_id`) REFERENCES `calendar_year` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `holidays`
+--
+
+LOCK TABLES `holidays` WRITE;
+/*!40000 ALTER TABLE `holidays` DISABLE KEYS */;
+INSERT INTO `holidays` VALUES (1,'New Year\'s Day',1,'2024-01-01','2024-01-01'),(2,'Martin Luther King Jr. Day',1,'2024-01-15','2024-01-15'),(3,'President’s Day',1,'2024-02-19','2024-02-19'),(4,'Memorial Day',1,'2024-05-27','2024-05-27'),(5,'Independence Day',1,'2024-07-04','2024-07-04'),(6,'Raspberry Days',1,'2024-08-01','2024-08-03'),(7,'Labor Day',1,'2024-09-02','2024-09-02'),(8,'Columbus Day',1,'2024-10-14','2024-10-14'),(9,'Thanksgiving Day',1,'2024-11-28','2024-11-28'),(10,'Christmas Day',1,'2024-12-25','2024-12-25'),(11,'New Year\'s Day',2,'2025-01-01','2025-01-01'),(12,'Martin Luther King Jr. Day',2,'2025-01-20','2025-01-20'),(13,'President’s Day',2,'2025-02-17','2025-02-17'),(14,'Memorial Day',2,'2025-05-26','2025-05-26'),(15,'Independence Day',2,'2025-07-04','2025-07-04'),(16,'Raspberry Days',2,'2025-08-01','2025-08-03'),(17,'Labor Day',2,'2025-09-01','2025-09-01'),(18,'Columbus Day',2,'2025-10-13','2025-10-13'),(19,'Thanksgiving Day',2,'2025-11-27','2025-11-27'),(20,'Christmas Day',2,'2025-12-25','2025-12-25'),(21,'New Year\'s Day',3,'2026-01-01','2026-01-01'),(22,'Martin Luther King Jr. Day',3,'2026-01-19','2026-01-19'),(23,'President’s Day',3,'2026-02-16','2026-02-16'),(24,'Memorial Day',3,'2026-05-25','2026-05-25'),(25,'Independence Day',3,'2026-07-03','2026-07-03'),(26,'Raspberry Days',3,'2026-08-01','2026-08-03'),(27,'Labor Day',3,'2026-09-07','2026-09-07'),(28,'Columbus Day',3,'2026-10-12','2026-10-12'),(29,'Thanksgiving Day',3,'2026-11-26','2026-11-26'),(30,'Christmas Day',3,'2026-12-25','2026-12-25');
+/*!40000 ALTER TABLE `holidays` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-07-23 12:25:55
---
+++ Dump20240723/fraxioned_peak_off_booking_rule.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 192.168.1.47    Database: fraxioned
+-- ------------------------------------------------------
+-- Server version	8.0.37
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `peak_off_booking_rule`
+--
+
+DROP TABLE IF EXISTS `peak_off_booking_rule`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `peak_off_booking_rule` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `minimum_stay_length` int NOT NULL DEFAULT '3',
+  `maximum_stay_length` int NOT NULL DEFAULT '14',
+  `maxstaylength_incremental_count` int DEFAULT NULL,
+  `nights_between_each_booking` int NOT NULL DEFAULT '5',
+  `is_cancellation_allowed` tinyint DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `peak_off_booking_rule`
+--
+
+LOCK TABLES `peak_off_booking_rule` WRITE;
+/*!40000 ALTER TABLE `peak_off_booking_rule` DISABLE KEYS */;
+INSERT INTO `peak_off_booking_rule` VALUES (1,3,14,7,5,1);
+/*!40000 ALTER TABLE `peak_off_booking_rule` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-07-23 12:25:58
