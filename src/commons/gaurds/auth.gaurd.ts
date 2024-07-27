90b4c379686f847c00c221aef4cd957f756f2131
JohnsonMelvin <126421095+JohnsonMelvin@users.noreply.github.com>
2024-07-27 17:58:12 +0530

    Auth Gaurd added

--- src/commons/gaurds/auth.gaurd.ts
+++ src/commons/gaurds/auth.gaurd.ts
@@ -1,36 +1,36 @@
-// import {
-//     Injectable,
-//     CanActivate,
-//     ExecutionContext,
-//     UnauthorizedException,
-//   } from '@nestjs/common';
-//   import { AuthenticationService } from 'src/service/authentication.service';
-//   @Injectable()
-//   export class AuthGuard implements CanActivate {
-//     constructor(private authService: AuthenticationService) {}
+import {
+    Injectable,
+    CanActivate,
+    ExecutionContext,
+    UnauthorizedException,
+  } from '@nestjs/common';
+  import { AuthenticationService } from 'src/service/authentication.service';
+  @Injectable()
+  export class AuthGuard implements CanActivate {
+    constructor(private authService: AuthenticationService) {}
 
-//     async canActivate(context: ExecutionContext): Promise<boolean> {
-//       const request = context.switchToHttp().getRequest();
-//       const userId = request.headers['user-id'];
-//       const accessToken = request.headers['access-token'];
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+      const request = context.switchToHttp().getRequest();
+      const userId = request.headers['user-id'];
+      const accessToken = request.headers['access-token'];
 
-//       if (!userId || !accessToken) {
-//         throw new UnauthorizedException(
-//           'Please provide a valid user ID and access token',
-//         );
-//       }
+      if (!userId || !accessToken) {
+        throw new UnauthorizedException(
+          'Please provide a valid user ID and access token',
+        );
+      }
 
-//       const isValid = await this.authService.validateUser(
-//         Number(userId),
-//         accessToken,
-//       );
+      const isValid = await this.authService.validateUser(
+        Number(userId),
+        accessToken,
+      );
 
-//       if (!isValid) {
-//         throw new UnauthorizedException(
-//           'The provided user ID or access token is invalid',
-//         );
-//       }
+      if (!isValid) {
+        throw new UnauthorizedException(
+          'The provided user ID or access token is invalid',
+        );
+      }
 
-//       return true;
-//     }
-//   }
+      return true;
+    }
+  }
