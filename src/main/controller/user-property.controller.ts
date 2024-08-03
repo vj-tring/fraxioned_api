8be0c48f74fa5086de4f15ed8d2ca96d6230af1b
JohnsonMelvin <126421095+JohnsonMelvin@users.noreply.github.com>
2024-08-03 16:48:44 +0530

    [FRAX-77] Auth guard added

--- src/main/controller/user-property.controller.ts
+++ src/main/controller/user-property.controller.ts
@@ -6,11 +6,13 @@ import {
   Body,
   Delete,
   Patch,
+  UseGuards,
 } from '@nestjs/common';
 import { UserPropertyService } from 'services/user-property.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiHeader, ApiTags } from '@nestjs/swagger';
 import { CreateUserPropertyDTO } from '../dto/requests/create-user-property.dto';
 import { UpdateUserPropertyDTO } from '../dto/requests/update-user-property.dto';
+import { AuthGuard } from '../commons/guards/auth.guard';
 
 @ApiTags('UserProperty')
 @Controller('v1/user-properties')
@@ -18,6 +20,13 @@ export class UserPropertyController {
   constructor(private readonly userPropertyService: UserPropertyService) {}
 
   @Post('user-property')
+  @UseGuards(AuthGuard)
+  @ApiHeader({ name: 'user-id', required: true, description: 'User ID' })
+  @ApiHeader({
+    name: 'access-token',
+    required: true,
+    description: 'Access Token',
+  })
   async createUserProperty(
     @Body() createUserPropertyDto: CreateUserPropertyDTO,
   ): Promise<object> {
@@ -25,16 +34,37 @@ export class UserPropertyController {
   }
 
   @Get()
+  @UseGuards(AuthGuard)
+  @ApiHeader({ name: 'user-id', required: true, description: 'User ID' })
+  @ApiHeader({
+    name: 'access-token',
+    required: true,
+    description: 'Access Token',
+  })
   async getUserProperties(): Promise<object> {
     return this.userPropertyService.getUserProperties();
   }
 
   @Get('user-property/:id')
+  @UseGuards(AuthGuard)
+  @ApiHeader({ name: 'user-id', required: true, description: 'User ID' })
+  @ApiHeader({
+    name: 'access-token',
+    required: true,
+    description: 'Access Token',
+  })
   async getUserPropertyById(@Param('id') id: number): Promise<object> {
     return this.userPropertyService.getUserPropertyById(id);
   }
 
   @Patch('user-property/:id')
+  @UseGuards(AuthGuard)
+  @ApiHeader({ name: 'user-id', required: true, description: 'User ID' })
+  @ApiHeader({
+    name: 'access-token',
+    required: true,
+    description: 'Access Token',
+  })
   async updateUserProperty(
     @Param('id') id: number,
     @Body() updateUserPropertyDto: UpdateUserPropertyDTO,
@@ -46,6 +76,13 @@ export class UserPropertyController {
   }
 
   @Delete('user-property/:id')
+  @UseGuards(AuthGuard)
+  @ApiHeader({ name: 'user-id', required: true, description: 'User ID' })
+  @ApiHeader({
+    name: 'access-token',
+    required: true,
+    description: 'Access Token',
+  })
   async deleteUserProperty(@Param('id') id: number): Promise<object> {
     return this.userPropertyService.deleteUserProperty(id);
   }
