5ca0a38463179f707bca47d7e24e7c13b75823fb
JohnsonMelvin <126421095+JohnsonMelvin@users.noreply.github.com>
2024-08-07 13:00:26 +0530

    [FRAX-91] Overall TEST coverage: around 80%

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './main/modules/database.module';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { HolidaysModule } from 'modules/holidays.module';
 import { PropertiesModule } from './main/modules/properties.module';
-import { typeOrmConfigAsync } from './main/database/typeorm.config';
 import { AuthenticationModule } from './main/modules/authentication.module';
 import { RoleModule } from './main/modules/role.module';
 import { PropertyDetailsModule } from './main/modules/property-details.module';
@@ -17,8 +14,6 @@ import { UserDocumentModule } from './main/modules/user-document.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
     DatabaseModule,
     HolidaysModule,
     PropertiesModule,
