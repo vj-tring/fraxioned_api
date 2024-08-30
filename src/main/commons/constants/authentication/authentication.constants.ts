b18cecf3da9308184c37efe9c2e6e5d5094fcb17
TK <tharunkumar.saravanan@tringapps.net>
2024-08-30 13:22:45 +0530

    commit

--- src/main/commons/constants/authentication/authentication.constants.ts
+++ src/main/commons/constants/authentication/authentication.constants.ts
@@ -1,4 +1,4 @@
-// Change Port Number to 5000 for Production - http://192.168.1.47:5173
+// Change Port Number to 5000 for Production(Dev - http://192.168.1.47:5173)
 export const authConstants = {
   hostname: 'http://localhost',
   port: '5173',
