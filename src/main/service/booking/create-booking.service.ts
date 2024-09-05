b1a4eee0f2c89de8efa59c0cf83edc63a8829598
JohnsonMelvin <126421095+JohnsonMelvin@users.noreply.github.com>
2024-09-05 19:10:00 +0530

    lint fix

--- src/main/service/booking/create-booking.service.ts
+++ src/main/service/booking/create-booking.service.ts
@@ -349,31 +349,31 @@ export class CreateBookingService {
 
   private async generateBookingId(propertyId: number): Promise<string> {
     const baseStartNumber = 1;
-  
+
     const propertyIdLength = Math.max(2, propertyId.toString().length);
-  
+
     const lastBooking = await this.bookingRepository.findOne({
       where: {},
       order: { createdAt: 'DESC' },
       select: ['bookingId'],
     });
-  
+
     const lastId = lastBooking
       ? parseInt(lastBooking.bookingId.slice(6 + propertyIdLength), 10)
       : baseStartNumber - 1;
-  
+
     const newId = lastId + 1;
     const incrementingNumberLength = Math.max(2, newId.toString().length);
-  
+
     const currentYear = new Date().getFullYear().toString();
-  
+
     const paddedPropertyId = propertyId
       .toString()
       .padStart(propertyIdLength, '0');
     const paddedNewId = newId
       .toString()
       .padStart(incrementingNumberLength, '0');
-  
+
     return `FX${currentYear}${paddedPropertyId}${paddedNewId}`;
   }
 }
