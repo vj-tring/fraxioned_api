b47ac7a1916dbe1b85cdc972454e06847b804585
harini1817 <harinisree2023@gmail.com>
2024-10-03 13:42:01 +0530

    reports

--- src/main/modules/reports.module.ts
+++ src/main/modules/reports.module.ts
@@ -4,9 +4,13 @@ import { BookingHistory } from '../entities/booking-history.entity';
 import { ReportsService } from '../service/reports.service';
 import { ReportsController } from '../controller/reports.controller';
 import { AuthenticationModule } from './authentication.module';
+import { UserProperties } from '../entities/user-properties.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BookingHistory]), AuthenticationModule],
+  imports: [
+    TypeOrmModule.forFeature([BookingHistory, UserProperties]),
+    AuthenticationModule,
+  ],
   controllers: [ReportsController],
   providers: [ReportsService],
 })
