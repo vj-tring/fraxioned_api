1367eaf68778074f04fb7bc83272774fe1331aa3
TK <tharunkumar.saravanan@tringapps.net>
2024-10-08 19:18:12 +0530

    "Updated the Entity and DTOs according to the new logic in Property Space Amenities module."

--- src/main/entities/property-amenities.entity.ts
+++ src/main/entities/property-amenities.entity.ts
@@ -1,23 +1,11 @@
-import {
-  CreateDateColumn,
-  Entity,
-  JoinColumn,
-  ManyToOne,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-} from 'typeorm';
+import { Entity, JoinColumn, ManyToOne } from 'typeorm';
 import { Property } from './property.entity';
 import { Amenities } from './amenities.entity';
-import { User } from './user.entity';
+import { BaseEntity } from '../commons/abstracts/base-entity';
+import { PropertySpace } from './property-space.entity';
 
 @Entity('fxn_property_amenities')
-export class PropertyAmenities {
-  @PrimaryGeneratedColumn({
-    name: 'id',
-    comment: 'This is a unique identifier',
-  })
-  id: number;
-
+export class PropertyAmenities extends BaseEntity {
   @ManyToOne(() => Property, (properties) => properties.id)
   @JoinColumn({
     name: 'property_id',
@@ -30,27 +18,11 @@ export class PropertyAmenities {
   })
   amenity: Amenities;
 
-  @ManyToOne(() => User, (user) => user.id)
-  @JoinColumn({
-    name: 'created_by',
-  })
-  createdBy: User;
-
-  @ManyToOne(() => User, (user) => user.id)
-  @JoinColumn({
-    name: 'updated_by',
-  })
-  updatedBy: User;
-
-  @CreateDateColumn({
-    name: 'created_at',
+  @ManyToOne(() => PropertySpace, (propertySpace) => propertySpace.id, {
     nullable: true,
   })
-  createdAt: Date = undefined;
-
-  @UpdateDateColumn({
-    name: 'updated_at',
-    nullable: true,
+  @JoinColumn({
+    name: 'property_space_id',
   })
-  updatedAt: Date = undefined;
+  propertySpace?: PropertySpace;
 }
