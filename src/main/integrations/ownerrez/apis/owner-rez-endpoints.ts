323e500bc481a1151d849e9990a11314dc50b2c1
TK <tharunkumar.saravanan@tringapps.net>
2024-10-18 19:48:45 +0530

    "Refactor the Owner Rez Endpoints code to get the sensitive information from the env file(implemented async and config service) and added the update functionality."

--- src/main/integrations/ownerrez/apis/owner-rez-endpoints.ts
+++ src/main/integrations/ownerrez/apis/owner-rez-endpoints.ts
@@ -1,18 +1,33 @@
-import axios, { AxiosResponse } from 'axios';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
 
-const username = 'jaiganesh.j@tringapps.com';
-const password = 'pt_oxy7rb429ic9bpvuva47vuuoqb5kjndp';
-
-const client = axios.create({
-  // PROD - https://api.ownerrez.com/
-  baseURL: 'https://apistage.ownerrez.com/',
-  auth: {
-    username,
-    password,
+export const axiosConfigAsync = {
+  imports: [ConfigModule],
+  useFactory: async (configService: ConfigService): Promise<AxiosInstance> => {
+    const axiosInstance = axios.create({
+      baseURL: configService.get<string>('API_URL'),
+      auth: {
+        username: configService.get<string>('API_USERNAME'),
+        password: configService.get<string>('API_PASSWORD'),
+      },
+    });
+    return axiosInstance;
   },
-});
+  inject: [ConfigService],
+};
+
+let dynamicClient: AxiosInstance | null = null;
+
+const getClient = async (): Promise<AxiosInstance> => {
+  if (!dynamicClient) {
+    const configService = new ConfigService();
+    dynamicClient = await axiosConfigAsync.useFactory(configService);
+  }
+  return dynamicClient;
+};
 
 export const getAllProperties = async (): Promise<AxiosResponse> => {
+  const client = await getClient();
   const response: AxiosResponse = await client.get('v2/properties');
   return response;
 };
@@ -20,6 +35,19 @@ export const getAllProperties = async (): Promise<AxiosResponse> => {
 export const createBooking = async (
   booking: object,
 ): Promise<AxiosResponse> => {
+  const client = await getClient();
   const response: AxiosResponse = await client.post('v2/bookings', booking);
+  return response.data;
+};
+
+export const updateBooking = async (
+  bookingId: number,
+  booking: object,
+): Promise<AxiosResponse> => {
+  const client = await getClient();
+  const response: AxiosResponse = await client.patch(
+    `v2/bookings/${bookingId}`,
+    booking,
+  );
   return response.data;
 };
