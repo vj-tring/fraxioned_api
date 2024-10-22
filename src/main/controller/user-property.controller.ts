538620547bcba2278da6dfb5a26178891693b8d4
JohnsonMelvin <126421095+JohnsonMelvin@users.noreply.github.com>
2024-10-22 23:20:54 +0530

    share use case in update update

--- src/main/controller/user-property.controller.ts
+++ src/main/controller/user-property.controller.ts
@@ -5,7 +5,7 @@ import {
   Param,
   Body,
   Delete,
-  // Patch,
+  Patch,
   UseGuards,
 } from '@nestjs/common';
 import { UserPropertyService } from 'services/user-property.service';
@@ -13,12 +13,12 @@ import { ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from 'commons/guards/auth.guard';
 import { ApiHeadersForAuth } from '../commons/guards/auth-headers.decorator';
 import { CreateUserPropertyDTO } from '../dto/requests/user-property/create-user-property.dto';
-// import { UpdateUserPropertyDTO } from '../dto/requests/user-property/update-user-property.dto';
+import { UpdateUserPropertyDTO } from '../dto/requests/user-property/update-user-property.dto';
 
 @ApiTags('UserProperty')
 @Controller('v1/user-properties')
-@UseGuards(AuthGuard)
-@ApiHeadersForAuth()
+// @UseGuards(AuthGuard)
+// @ApiHeadersForAuth()
 export class UserPropertyController {
   constructor(private readonly userPropertyService: UserPropertyService) {}
 
@@ -37,6 +37,13 @@ export class UserPropertyController {
   @Get('user-property/:id')
   async getUserPropertyById(@Param('id') id: number): Promise<object> {
     return this.userPropertyService.getUserPropertyById(id);
+  }
+
+  @Patch('user-property/:id')
+  async updateUserProperty(
+    @Body() updateUserPropertyDto: UpdateUserPropertyDTO,
+  ): Promise<object> {
+    return this.userPropertyService.updateUserProperty(updateUserPropertyDto);
   }
 
   @Delete('user-property/user/:userId/property/:propertyId')
--- src/main/utils/user-property.util.ts
+++ src/main/utils/user-property.util.ts
@@ -7,6 +7,218 @@ import { PropertyRepository } from '../repository/property.repository';
 import { UserPropertyRepository } from '../repository/user-property.repository';
 import { PropertyDetailsRepository } from '../repository/property-details.repository';
 
+export async function updateAvailableNightsForUserByProperty(
+  userPropertyDetails: UserPropertyDto[],
+  user: User,
+  propertyRepository: PropertyRepository,
+  propertyDetailsRepository: PropertyDetailsRepository,
+  userPropertyRepository: UserPropertyRepository,
+  logger: LoggerService,
+  existingUserProperties?: UserProperties[],
+  createdByUser?: User,
+  updatedByUser?: User,
+): Promise<UserProperties[] | object> {
+  const currentYear = new Date().getFullYear();
+  const userPropertyEntities: UserProperties[] = [];
+
+  for (const propertyDetail of userPropertyDetails) {
+    const propertyId = propertyDetail.propertyID;
+
+    const userProperty = await propertyRepository.findProperty(propertyId);
+    const userPropertyDetails =
+      await propertyDetailsRepository.findOne(propertyId);
+
+    if (!userProperty || !userPropertyDetails) {
+      logger.error(`Property or detail not found with ID: ${propertyId}`);
+      return !userProperty
+        ? USER_PROPERTY_RESPONSES.PROPERTY_NOT_FOUND(propertyId)
+        : USER_PROPERTY_RESPONSES.PROPERTY_DETAIL_NOT_FOUND(propertyId);
+    }
+
+    let shareDifference = 0;
+    if (existingUserProperties && existingUserProperties.length > 0) {
+      const oldShares = existingUserProperties[0].noOfShare;
+      const newShares = propertyDetail.noOfShares;
+      shareDifference = newShares - oldShares;
+
+      if (shareDifference !== 0) {
+        if (
+          shareDifference > 0 &&
+          shareDifference > userProperty.propertyRemainingShare
+        ) {
+          logger.error(
+            `Not enough remaining shares for property ID: ${propertyId}. Need ${shareDifference} more shares but only ${userProperty.propertyRemainingShare} available.`,
+          );
+          return USER_PROPERTY_RESPONSES.INSUFFICIENT_SHARES(
+            propertyId,
+            userProperty.propertyRemainingShare,
+          );
+        }
+
+        userProperty.propertyRemainingShare -= shareDifference;
+        await propertyRepository.saveProperty(userProperty);
+      }
+    } else {
+      if (propertyDetail.noOfShares > userProperty.propertyRemainingShare) {
+        logger.error(
+          `Not enough remaining shares for property ID: ${propertyId}`,
+        );
+        return USER_PROPERTY_RESPONSES.INSUFFICIENT_SHARES(
+          propertyId,
+          userProperty.propertyRemainingShare,
+        );
+      }
+
+      userProperty.propertyRemainingShare -= propertyDetail.noOfShares;
+      await propertyRepository.saveProperty(userProperty);
+    }
+
+    const acquisitionDate = new Date(propertyDetail.acquisitionDate);
+    const isAcquisitionInCurrentYear =
+      acquisitionDate.getFullYear() === currentYear;
+
+    const peakAllottedNights = calculateAllottedNightsForUser(
+      propertyDetail.noOfShares,
+      userPropertyDetails.peakSeasonAllottedNights,
+    );
+    const offAllottedNights = calculateAllottedNightsForUser(
+      propertyDetail.noOfShares,
+      userPropertyDetails.offSeasonAllottedNights,
+    );
+    const peakAllottedHolidayNights = calculateAllottedNightsForUser(
+      propertyDetail.noOfShares,
+      userPropertyDetails.peakSeasonAllottedHolidayNights,
+    );
+    const offAllottedHolidayNights = calculateAllottedNightsForUser(
+      propertyDetail.noOfShares,
+      userPropertyDetails.offSeasonAllottedHolidayNights,
+    );
+
+    const ratedPeakAllottedNights =
+      isAcquisitionInCurrentYear && shareDifference > 0
+        ? calculateAllottedNightsForUser(
+            shareDifference,
+            userPropertyDetails.peakSeasonAllottedNights,
+          )
+        : rateNights(peakAllottedNights, acquisitionDate);
+
+    const ratedOffAllottedNights =
+      isAcquisitionInCurrentYear && shareDifference > 0
+        ? calculateAllottedNightsForUser(
+            shareDifference,
+            userPropertyDetails.offSeasonAllottedNights,
+          )
+        : rateNights(offAllottedNights, acquisitionDate);
+
+    const ratedPeakHolidayNights =
+      isAcquisitionInCurrentYear && shareDifference > 0
+        ? calculateAllottedNightsForUser(
+            shareDifference,
+            userPropertyDetails.peakSeasonAllottedHolidayNights,
+          )
+        : rateNights(peakAllottedHolidayNights, acquisitionDate);
+
+    const ratedOffHolidayNights =
+      isAcquisitionInCurrentYear && shareDifference > 0
+        ? calculateAllottedNightsForUser(
+            shareDifference,
+            userPropertyDetails.offSeasonAllottedHolidayNights,
+          )
+        : rateNights(offAllottedHolidayNights, acquisitionDate);
+
+    const maximumStayLength = Math.min(
+      14 + (propertyDetail.noOfShares - 1) * 7,
+      28,
+    );
+
+    for (let yearOffset = 0; yearOffset <= 3; yearOffset++) {
+      const year = currentYear + yearOffset;
+      const isCurrentYear = year === currentYear;
+
+      const peakSeasonEndDate = new Date(userPropertyDetails.peakSeasonEndDate);
+      const isAfterPeakSeasonEndDate = acquisitionDate > peakSeasonEndDate;
+
+      let currentYearPeakNights = peakAllottedNights;
+      let currentYearOffNights = offAllottedNights;
+      let currentYearPeakHolidayNights = peakAllottedHolidayNights;
+      let currentYearOffHolidayNights = offAllottedHolidayNights;
+
+      if (
+        isCurrentYear &&
+        existingUserProperties &&
+        existingUserProperties.length > 0
+      ) {
+        const existingProperty = existingUserProperties.find(
+          (p) => p.year === currentYear,
+        );
+        if (existingProperty) {
+          currentYearPeakNights = isAfterPeakSeasonEndDate
+            ? existingProperty.peakRemainingNights
+            : existingProperty.peakRemainingNights + ratedPeakAllottedNights;
+          currentYearOffNights =
+            existingProperty.offRemainingNights + ratedOffAllottedNights;
+          currentYearPeakHolidayNights = isAfterPeakSeasonEndDate
+            ? existingProperty.peakRemainingHolidayNights
+            : existingProperty.peakRemainingHolidayNights +
+              ratedPeakHolidayNights;
+          currentYearOffHolidayNights =
+            existingProperty.offRemainingHolidayNights + ratedOffHolidayNights;
+        }
+      } else if (isCurrentYear) {
+        currentYearPeakNights = isAfterPeakSeasonEndDate
+          ? 0
+          : ratedPeakAllottedNights;
+        currentYearOffNights = ratedOffAllottedNights;
+        currentYearPeakHolidayNights = isAfterPeakSeasonEndDate
+          ? 0
+          : ratedPeakHolidayNights;
+        currentYearOffHolidayNights = ratedOffHolidayNights;
+      }
+
+      userPropertyEntities.push(
+        userPropertyRepository.create({
+          property: userProperty,
+          noOfShare: propertyDetail.noOfShares,
+          acquisitionDate: acquisitionDate,
+          user,
+          year,
+          createdBy: createdByUser,
+          updatedBy: updatedByUser,
+          peakAllottedNights: isCurrentYear
+            ? currentYearPeakNights
+            : peakAllottedNights,
+          peakRemainingNights: isCurrentYear
+            ? currentYearPeakNights
+            : peakAllottedNights,
+          peakAllottedHolidayNights: isCurrentYear
+            ? currentYearPeakHolidayNights
+            : peakAllottedHolidayNights,
+          peakRemainingHolidayNights: isCurrentYear
+            ? currentYearPeakHolidayNights
+            : peakAllottedHolidayNights,
+          offAllottedNights: isCurrentYear
+            ? currentYearOffNights
+            : offAllottedNights,
+          offRemainingNights: isCurrentYear
+            ? currentYearOffNights
+            : offAllottedNights,
+          offAllottedHolidayNights: isCurrentYear
+            ? currentYearOffHolidayNights
+            : offAllottedHolidayNights,
+          offRemainingHolidayNights: isCurrentYear
+            ? currentYearOffHolidayNights
+            : offAllottedHolidayNights,
+          lastMinuteAllottedNights: 8 * propertyDetail.noOfShares,
+          lastMinuteRemainingNights: 8 * propertyDetail.noOfShares,
+          maximumStayLength: maximumStayLength,
+        } as UserProperties),
+      );
+    }
+  }
+
+  return userPropertyEntities;
+}
+
 export async function calculateAvailableNightsForUserByProperty(
   userPropertyDetails: UserPropertyDto[],
   user: User,
